635bc37dd6e76d77ac57ac5bb83e3eedfc86c868
KenWhite02 <whiteken2002@gmail.com>
2021-12-14 20:57:16 +0300

    Added Home component

---
+++ components/Home/Banner.jsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import banner from '../../assets/pexels-pixabay-301692.jpg';
+
+const Banner = () => {
+  return (
+    <div className="banner w-full relative">
+      <Image
+        src={banner}
+        alt="banner"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+        quality={100}
+        priority
+      />
+      <div className="absolute top-1/2 text-white mx-2">
+        <h2 className="text-4xl font-bold font-mono md:text-5xl lg:text-6xl">
+          Welcome,
+        </h2>
+        <h2 className="text-2xl md:text-3xl lg:text-4xl">
+          Browse and explore drinks from around the world.
+        </h2>
+      </div>
+    </div>
+  );
+};
+export default Banner;
---
+++ components/Home/Popular.jsx
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+
+const Popular = ({ drinks }) => {
+  const cocktails = drinks.slice(0, 9);
+
+  return (
+    <div>
+      <h2 className="text-black font-bold text-4xl mt-5 text-center">
+        Popular drinks
+      </h2>
+
+      <div className="m-2 sm:grid md:grid-cols-2 lg:grid-cols-3">
+        {cocktails.map((cocktail) => {
+          const { idDrink, strDrink, strAlcoholic, strGlass, strDrinkThumb } =
+            cocktail;
+          return (
+            <div
+              key={idDrink}
+              className="group shadow-2xl cursor-pointer my-10 mx-3 bg-white transition-all duration-500 ease-in-out hover:scale-105"
+            >
+              <Image
+                src={strDrinkThumb}
+                alt={strDrink}
+                layout="responsive"
+                placeholder="blur"
+                blurDataURL={strDrinkThumb}
+                height={450}
+                width={500}
+                objectFit="cover"
+              />
+
+              <div className="p-2 text-black">
+                <h2 className="font-extrabold text-4xl">{strDrink}</h2>
+                <h2 className="font-mono">{strGlass}</h2>
+                <p className="text-gray-400">{strAlcoholic}</p>
+                <button className="bg-green-500 px-4 py-2 tracking-widest text-lg opacity-0 transition-all duration-1000 ease-in-out group-hover:opacity-100">
+                  Details
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Popular;
